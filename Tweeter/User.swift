b1fae827ba17d93b825417335db995e47c92bc92
Kyle Sit <Kyle@kyles-macbook-pro-4.local>
2017-02-23 19:28:06 -0800

    Models made

---
+++ Tweeter/User.swift
@@ -0,0 +1,29 @@
+//
+//  User.swift
+//  Tweeter
+//
+//  Created by Kyle Sit on 2/23/17.
+//  Copyright © 2017 Kyle Sit. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+    var name: NSString?
+    var screenName: NSString?
+    var profileURL: NSURL?
+    var tagline: NSString?
+    
+    init(dictionary: NSDictionary) {
+        name = dictionary["name"] as? String as NSString?
+        screenName = dictionary["screen_name"] as? String as NSString?
+        tagline = dictionary["description"] as? String as NSString?
+        
+        let profileURLString = dictionary["profile_image_url_https"] as? String
+        if let profileURLString = profileURLString {
+            profileURL = NSURL(string: profileURLString)
+        }
+    }
+    
+}
