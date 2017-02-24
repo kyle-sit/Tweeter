b3143807c516cdae3fff8db351132baeeb960830
Kyle Sit <Kyle@kyles-macbook-pro-4.local>
2017-02-23 16:20:37 -0800

    Initial Commit

---
+++ Tweeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweeter
+//
+//  Created by Kyle Sit on 2/23/17.
+//  Copyright © 2017 Kyle Sit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
